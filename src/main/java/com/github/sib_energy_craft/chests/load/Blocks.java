31648b2167722bc3b70ae83619799b7285b98b37
sibmaks <sibmaks@vk.com>
2023-06-02 16:17:16 +0300

    Update Minecraft up to 1.20

--- src/main/java/com/github/sib_energy_craft/chests/load/Blocks.java
+++ src/main/java/com/github/sib_energy_craft/chests/load/Blocks.java
@@ -5,7 +5,7 @@ import com.github.sib_energy_craft.energy_api.utils.Identifiers;
 import com.github.sib_energy_craft.sec_utils.common.Identified;
 import com.github.sib_energy_craft.sec_utils.load.DefaultModInitializer;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.MapColor;
 import net.minecraft.sound.BlockSoundGroup;
 
 import static com.github.sib_energy_craft.sec_utils.utils.BlockUtils.register;
@@ -23,25 +23,49 @@ public final class Blocks implements DefaultModInitializer {
     public static final Identified<DiamondExtendedChestBlock> DIAMOND_CHEST;
 
     static {
-        var chestSettings = AbstractBlock.Settings
-                .of(Material.METAL)
+        var ironChestSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.IRON_GRAY)
+                .strength(2F)
+                .requiresTool()
+                .sounds(BlockSoundGroup.METAL);
+
+        var ironExtendedChestBlock = new IronExtendedChestBlock(ironChestSettings);
+        IRON_CHEST = register(Identifiers.of("iron_chest"), ironExtendedChestBlock);
+
+        var copperChestSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.ORANGE)
                 .strength(2.5F)
                 .requiresTool()
                 .sounds(BlockSoundGroup.METAL);
 
-        var ironExtendedChestBlock = new IronExtendedChestBlock(chestSettings);
-        IRON_CHEST = register(Identifiers.of("iron_chest"), ironExtendedChestBlock);
-
-        var copperExtendedChestBlock = new CopperExtendedChestBlock(chestSettings);
+        var copperExtendedChestBlock = new CopperExtendedChestBlock(copperChestSettings);
         COPPER_CHEST = register(Identifiers.of("copper_chest"), copperExtendedChestBlock);
 
-        var goldExtendedChestBlock = new GoldExtendedChestBlock(chestSettings);
+        var goldChestSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.GOLD)
+                .strength(3F)
+                .requiresTool()
+                .sounds(BlockSoundGroup.METAL);
+
+        var goldExtendedChestBlock = new GoldExtendedChestBlock(goldChestSettings);
         GOLD_CHEST = register(Identifiers.of("gold_chest"), goldExtendedChestBlock);
 
-        var silverExtendedChestBlock = new SilverExtendedChestBlock(chestSettings);
+        var silverChestSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.LIGHT_GRAY)
+                .strength(3.5F)
+                .requiresTool()
+                .sounds(BlockSoundGroup.METAL);
+
+        var silverExtendedChestBlock = new SilverExtendedChestBlock(silverChestSettings);
         SILVER_CHEST = register(Identifiers.of("silver_chest"), silverExtendedChestBlock);
 
-        var diamondExtendedChestBlock = new DiamondExtendedChestBlock(chestSettings);
+        var diamondChestSettings = AbstractBlock.Settings.create()
+                .mapColor(MapColor.DIAMOND_BLUE)
+                .strength(4.5F)
+                .requiresTool()
+                .sounds(BlockSoundGroup.METAL);
+
+        var diamondExtendedChestBlock = new DiamondExtendedChestBlock(diamondChestSettings);
         DIAMOND_CHEST = register(Identifiers.of("diamond_chest"), diamondExtendedChestBlock);
     }
 }
